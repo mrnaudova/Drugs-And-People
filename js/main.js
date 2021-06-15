3a7efa93c2b7befe8fc322281e700fda39489bfd
mrnaudova <mrnaudova8g@gmail.com>
2021-06-15 11:29:03 +0300

    added folder images and contact-form(uncompleted)

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ function changeBackground(color) {
  }
  
  window.addEventListener("load",function() { changeBackground('#57595a') });
+
+ (function() {
+     'use strict';
+     window.addEventListener('load', function() {
+         var forms = this.document.getElementsByClassName('need-validation');
+         var validation = Array.prototype.filter.call(forms, function(form) {
+             form.addEventListener('simbit', function(event) {
+                 if (form.checkValidity() === false) {
+                     event.preventDefault();
+                     event.stopPropagation();
+                 }
+                 form.classList.add('was-validated');
+                 }, false);
+                });
+        }, false);
+    })();
+ 
